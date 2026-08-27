a2770a77016ae99024e4f5e0e27194ebed8da6ac
epicadi7 <adimule37@gmail.com>
2026-08-27 09:07:38 +0530

    Update page.js

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -3,6 +3,7 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { supabase } from "../../src/lib/supabase";
+import Link from "next/link";
 
 export default function Dashboard() {
   const router = useRouter();
